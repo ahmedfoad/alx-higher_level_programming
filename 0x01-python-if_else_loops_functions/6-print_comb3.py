ba3bd44bba6796e78ace4ac2a2894a70772a0d0d
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-06-06 15:02:21 +0300

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for first_digit in range(0, 9):
-    for secound_digit in range(0, 10):
-        if first_digit == 8 and secound_digit == 9:
-            print(f"{first_digit}{secound_digit}")
-        elif secound_digit > first_digit:
-            print(f"{first_digit}{secound_digit}, ", end="")
+for fst_digit in range(0, 10):
+    for sec_digit in range(fst_digit + 1, 10):
+        if fst_digit == 8 and sec_digit == 9:
+            print("{}{}".format(fst_digit, sec_digit))
+        else:
+            print("{}{}".format(fst_digit, sec_digit), end=", ")
