a78fec4df65da92be17d04f1d986939f5aeaa164
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-06-06 14:26:37 +0300

    4. Hexadecimal printing

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for counter in range(0, 99):
+    decimal_number = counter
+    hexadecimal_number = hex(counter)
+    print(f"{decimal_number} = {hexadecimal_number}\n", end=" ")
