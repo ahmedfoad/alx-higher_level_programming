6495e22316fa94cf7587287adec5c378e30806bd
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-06-10 18:05:35 +0300

    using str.format()

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,4 @@
 # def print_list_integer(lst): prints all the integers in a given list
 def print_list_integer(my_list):
     for num in my_list:
-        if type(num) == int:
-            print(num)
+        print("{:d}".format(num))
