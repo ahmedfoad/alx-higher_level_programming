c3a1b13375adb92460e528654bedda9123ccde06
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-06-10 20:02:37 +0300

    fix return none

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@
 # element_at(my_list, idx): a function that retrieves an element from a list like in C
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        return None
+        return 'None'
     return my_list[idx]
