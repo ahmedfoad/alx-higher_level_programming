983cfd5faf78a775c511c59984c90ac93d4c4898
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-06-10 20:13:39 +0300

    fix element_At function

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-# element_at(my_list, idx): a function that
-# retrieves an element from a list like in C
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return 'None'
     else:
         return my_list[idx]
+
 
