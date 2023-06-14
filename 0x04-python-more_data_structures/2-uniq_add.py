d42965478a8060cce415b65cf9baaf9ba4d7c222
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-06-14 23:05:13 +0300

    2. Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    uniq_list = list(set(my_list))
+    for item in uniq_list:
+        result += item
+    
+    return (result)        
