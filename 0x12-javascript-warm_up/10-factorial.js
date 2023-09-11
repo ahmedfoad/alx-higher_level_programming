a2a305e956146360c547e6b680cff5e2493ae684
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-09-12 00:21:54 +0300

    betty

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,16 @@
-#!/usr/bin/node
+/**
+ * Calculates the factorial of a number.
+ * @param {number} n - The number to calculate the factorial for.
+ * @return {number} The factorial of the input number.
+ */
 function factorial(n) {
-    if (n < 0) {
-        return (-1);
-    }
-    if (n === 0 || isNaN(n)) {
-        return (1);
-    }
-    return (n * factorial(n - 1));
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
 }
 
 console.log(factorial(Number(process.argv[2])));
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,8 @@
-#!/usr/bin/node
+/**
+ * Adds two numbers and prints the result to the console.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ */
 function add(a, b) {
   if (!isNaN(process.argv[2]) && !isNaN(process.argv[3])) {
     const sum = a + b;
@@ -9,4 +13,3 @@ function add(a, b) {
 }
 
 add(parseInt(process.argv[2]), parseInt(process.argv[3]));
-
